93f2dde007e414a49ff53aa26a11693dc87b5abc
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-01-30 15:06:56 -0300

    Morgan/Nodemon/Tratar Rotas inexistentes

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+// Responsável por registrar os logs das requisições em desenvolvimento
+app.use(morgan('dev'));
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+
+app.use((req, res, next)=>{
+    const erro = new Error('não encontrado');
+    erro.status =404;
+    next(erro);
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    return res.send({
+        erro:{
+            mensagem: `Erro: ${error.mensagem}`
+        }
+    })
+})
 
 module.exports = app;
