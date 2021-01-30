9bb1860a4ffbc438364ec1ab857bacd8f7767811
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-01-30 11:17:11 -0300

    Montando ambiente

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    res.status(200).send({
+        message: 'ok'
+    });
+
+});
+
+module.exports = app;
